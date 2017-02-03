6ed394eda315de5551a06984dbe8679403283173
Megan Jackson <meganjackson@ufl.edu>
2017-02-03 10:59:13 -0800

    Add 5 usPresident objects to main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,52 @@
 var iceCream = [
 
-		"chocolate",
-		"mint chocolate chip",
-		"pistachio"
-		"cookies and cream"
+	"chocolate",
+	"mint chocolate chip",
+	"pistachio",
+	"cookies and cream"
+];
 
-	];
+var usPresident1 = {
+
+	firstName: "Donald",
+	lastName: "Trump",
+	termLength: "1",
+	party: "Republican",
+	yearsOfPresidency: "0"
+};
+
+var usPresident2 = {
+
+	firstName: "Barack",
+	lastName: "Obama",
+	termLength: "2",
+	party: "Democrat",
+	yearsOfPresidency: "8"
+};
+
+var usPresident3 = {
+
+	firstName: "George W.",
+	lastName: "Bush",
+	termLength: "2",
+	party: "Republican",
+	yearsOfPresidency: "8"
+};
+
+var usPresident4 = {
+
+	firstName: "Bill",
+	lastName: "Clinton",
+	termLength: "2",
+	party: "Democrat",
+	yearsOfPresidency: "8"
+};
+
+var usPresident5 = {
+
+	firstName: "George",
+	lastName: "Bush",
+	termLength: "1",
+	party: "Republican",
+	yearsOfPresidency: "4"
+};
