7ab69be9a1723161cf9eee65a913fc75d341358c
Megan Jackson <meganjackson@ufl.edu>
2017-02-03 13:04:24 -0800

    Added console.log() to log out third president, George W. Bush

--- js/main.js
+++ js/main.js
@@ -1,50 +1,52 @@
 var iceCream = [
 
-	"chocolate",
-	"mint chocolate chip",
-	"pistachio",
-	"cookies and cream"
+	'chocolate',
+	'mint chocolate chip',
+	'pistachio',
+	'cookies and cream'
 ];
 
 var presidents = [
 
 	 {
-		firstName: "Donald",
-		lastName: "Trump",
-		termLength: "1",
-		party: "Republican",
-		yearsOfPresidency: "0",
+		firstName: 'Donald',
+		lastName: 'Trump',
+		termLength: '1',
+		party: 'Republican',
+		yearsOfPresidency: '0',
 	},
 
 	{
-		firstName: "Barack",
-		lastName: "Obama",
-		termLength: "2",
-		party: "Democrat",
-		yearsOfPresidency: "8",
+		firstName: 'Barack',
+		lastName: 'Obama',
+		termLength: '2',
+		party: 'Democrat',
+		yearsOfPresidency: '8',
 	},
 
 	 {
-		firstName: "George W.",
-		lastName: "Bush",
-		termLength: "2",
-		party: "Republican",
-		yearsOfPresidency: "8",
+		firstName: 'George W.',
+		lastName: 'Bush',
+		termLength: '2',
+		party: 'Republican',
+		yearsOfPresidency: '8',
 	},
 
 	{
-		firstName: "Bill",
-		lastName: "Clinton",
-		termLength: "2",
-		party: "Democrat",
-		yearsOfPresidency: "8",
+		firstName: 'Bill',
+		lastName: 'Clinton',
+		termLength: '2',
+		party: 'Democrat',
+		yearsOfPresidency: '8',
 	},
 
 	{
-		firstName: "George",
-		lastName: "Bush",
-		termLength: "1",
-		party: "Republican",
-		yearsOfPresidency: "4",
+		firstName: 'George',
+		lastName: 'Bush',
+		termLength: '1',
+		party: 'Republican',
+		yearsOfPresidency: '4',
 	},
 ];
+
+console.log(presidents[2]['firstName'] + ' ' + presidents[2]['lastName']);
